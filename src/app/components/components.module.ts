64c84f08b336279845e38580b427813d573c2621
akifkomurcu <akif.tonmaister58@gmail.com>
2024-09-08 23:41:28 +0300

    ilk build için son düzenleme

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { BioComponent } from './bio/bio.component';
 import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [ HeaderComponent, BioComponent ],
+  declarations: [ HeaderComponent, BioComponent, HomeComponent ],
   imports: [ CommonModule, RouterModule ],
-  exports: [ HeaderComponent, BioComponent ],
+  exports: [ HeaderComponent, BioComponent, HomeComponent ],
   providers: [],
 })
 export class ComponentsModule {}
